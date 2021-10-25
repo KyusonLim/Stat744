b24f064632d75beecb62b90e0d7ed502e4ff62a5
Ben Bolker <bbolker@gmail.com>
2021-10-25 12:34:56 -0400

    hw4 comments

--- hw4.R
+++ hw4.R
@@ -3,18 +3,18 @@
 # Part 2. Modelling using shiny for COVID-19 death for provinces
 
 # Motivation: In the era of big data, the uncertainty of how much data we can analyze is a question of interest for many people.
-# However, the importance of data mining and the techniques resolves the qustion of interest for many people to comprehend big data by crawling from the internet instantaneously. 
-# It not only enables many people to model the unique qualitative data by text mining (using NLP) but also resolves the quatitative questions by creating a new and rapid interative models. 
+# However, the importance of data mining and the techniques resolves the qustion of interest for many people to comprehend big data by crawling from the internet instantaneously.
+# It not only enables many people to model the unique qualitative data by text mining (using NLP) but also resolves the quatitative questions by creating a new and rapid interative models.
 # Now, we are going to look at sophisticated new modelling LDA(latent Dirichlet allocation) for topic modelling about issues of COVID-19 from Statistics Canada website using crawling but also analyze the instantaneous everyday updates of COVID-19 death of each province.
 
-
-rm(list=ls())
+### BMB: don't do this. It implies you're not running a clean pipeline (i.e., restarting from a clean R session)
+## rm(list=ls())
 
 ## load the packages that are installed to help you run the coding
 library(rvest)
 library(dplyr)
 library(devtools)
-library(wordcloud2)
+library(wordcloud2)  ## BMB: why???
 library(plyr)
 library(tokenizers)
 library(lda)
@@ -24,12 +24,11 @@ library(tm)
 library(shiny)
 library(leaflet)
 library(RColorBrewer)
-library(rvest)
-library(RColorBrewer)
-library(sp)     
-library(leaflet.extras)      
-library(raster)      
-library(rgdal) 
+library(rvest)  ## BMB: repeated?
+library(sp)
+library(leaflet.extras)
+library(raster)
+library(rgdal)
 
 
 # this is the website, you are setting the website as a variable
@@ -52,7 +51,7 @@ lin		### there would be 90 headlines, all printed
 
 
 # pre-processing:
-lin <- gsub("'", "", lin); lin <- gsub("[[:punct:]]", " ", lin) 
+lin <- gsub("'", "", lin); lin <- gsub("[[:punct:]]", " ", lin)
 lin <- gsub("[[:cntrl:]]", " ", lin); lin <- gsub("^[[:space:]]+", "", lin)
 lin <- gsub("[[:space:]]+$", "", lin); lin <- tolower(lin)  # force to lowercase
 
@@ -80,17 +79,18 @@ documents <- lapply(doc.list, get.terms)
 
 # Compute some statistics related to the data set:
 D <- length(documents)  # number of documents
-W <- length(vocab)  # number of terms in the vocab 
-doc.length <- sapply(documents, function(x) sum(x[2, ]))  # number of tokens per document 
-N <- sum(doc.length)  # total number of tokens 
-term.frequency <- as.integer(term.table)  # frequencies of terms 
+W <- length(vocab)  # number of terms in the vocab
+doc.length <- sapply(documents, function(x) sum(x[2, ]))  # number of tokens per document
+N <- sum(doc.length)  # total number of tokens
+term.frequency <- as.integer(term.table)  # frequencies of terms
 
 
-## some self-filering 
+## some self-filering
 w7<-as.data.frame(term.table)[-1,]
-## this is wordcloud visualization 
+## this is wordcloud visualization
 wordcloud2(w7,size=2.5,color=c('skyblue'),gridSize=20,shape='circle',ellipticity=0.42)
 
+## BMB: this is cute, but it's _definitely_ not an inferential plot.
 
 
 # MCMC and model tuning parameters:
@@ -98,7 +98,7 @@ K <- 10;G <- 5000;alpha <- 0.02;eta <- 0.02
 
 # Fit the model:
 set.seed(744)
-fit <- lda.collapsed.gibbs.sampler(documents = documents, K = K, vocab = vocab, 
+fit <- lda.collapsed.gibbs.sampler(documents = documents, K = K, vocab = vocab,
         num.iterations = G, alpha = alpha, eta = eta, initial = NULL, burnin = 0, compute.log.likelihood = TRUE)
 
 
@@ -111,7 +111,7 @@ lin2 <- list(phi = phi, theta = theta, doc.length = doc.length,
 
 
 # create the JSON object to feed the visualization:
-json <- createJSON(phi = lin2$phi, theta = lin2$theta, doc.length = lin2$doc.length, 
+json <- createJSON(phi = lin2$phi, theta = lin2$theta, doc.length = lin2$doc.length,
                    vocab = lin2$vocab, term.frequency = lin2$term.frequency)
 
 ## LDA 'NLP' visualization
@@ -121,12 +121,14 @@ serVis(json, out.dir = 'vis', open.browser = T)
 
 
 # This shows from the statistics Canada headlines for qualitative point of view how we are to understand the issues of COVID-19 in Canada.
-# The topic modelling conveys popular topics of 10 keywords with explained sub-key words for each of the topics. 
-# Also, the wordcloud conveys from frequency based visualization for intuitive view to readers for simple and explicit words counts from the headlines. 
+# The topic modelling conveys popular topics of 10 keywords with explained sub-key words for each of the topics.
+# Also, the wordcloud conveys from frequency based visualization for intuitive view to readers for simple and explicit words counts from the headlines.
 # From both graphs, we are to observe the ideas and issues from official website for COVID-19 about what keywords and important ideas people have in past year 2020.
-# However, with shiny interactive app from official statistics Canada for how many COVID-19 people dies we could understand quantitively for the effect of COVID-19 by the data mining models and techniques. 
+# However, with shiny interactive app from official statistics Canada for how many COVID-19 people dies we could understand quantitatively for the effect of COVID-19 by the data mining models and techniques.
 
 
+## BMB: I'm still not sure what this tells us.  In this particular case,
+## (1) I would probably filter out "covid", "19", and possibly "canada", "canadian" from the corpus; (2) I'm not sure this corpus is big enough to be useful (there are only 24 distinct terms, with 346 total occurrences
 
 # Mining in Geospatial analysis
 # fetch data
@@ -192,28 +194,28 @@ ui <- bootstrapPage(
 server <- function(input, output, session) {
   filteredData <- reactive({
     fin[fin$`Total Deaths` >= input$range[1] & fin$`Total Deaths` <= input$range[2],]})
-  
+
   # This reactive expression represents the palette function
   colorpal <- reactive({colorNumeric(input$colors, fin$`Total Deaths`)})
-  
+
   output$map <- renderLeaflet({
     leaflet(fin) %>% addTiles() %>%
       fitBounds(~min(lng), ~min(lat), ~max(lng), ~max(lat))})
-  
-  # Incremental changes to the map 
+
+  # Incremental changes to the map
   observe({
     pal <- colorpal()
-    
+
     leafletProxy("map", data = filteredData()) %>%
       clearShapes() %>%
        #Layers control
       addCircles(radius = ~`Total Deaths`*70, weight = 1, color = "#FFFFCC",
                 fillColor = ~pal(`Total Deaths`), fillOpacity = 0.5, popup = ~paste(`Total Deaths`))})
-  
+
   # Use a separate observer to recreate the legend as needed.
   observe({
     proxy <- leafletProxy("map", data = fin)
-    
+
     # Remove any existing legend
     proxy %>% clearControls()
     if (input$legend) {pal <- colorpal()
@@ -227,13 +229,21 @@ shinyApp(ui, server)
 # From both quantitive graph and qualitative graphs of COVID-19, we are able to comprehend how the effect was for people by the COVID-19.
 # The quantitative shiny graph delivers for interactive graphs of how many peple died from con-current point of view which delivers the influence and hazard frequencies of COVID-19.
 # However, the qualitative graph of LDA for topic modeling in key words and sub-words explains the impact of COVID-19 to mass media and people for the issues.
-# Combined for both modeling, the model emphasizes the keen and comprehensive understand of on-going hazard information for COVID-19 to people by the method of data mining. 
+# Combined for both modeling, the model emphasizes the keen and comprehensive understand of on-going hazard information for COVID-19 to people by the method of data mining.
 
 # Conclusion:
 # The crawling of text data enables to comprehend beauty of words data and resolves what issues exist among headlines from the statistics Canada website.
-# Also, the wordcloud delivers for the beauty of frequncies and its intuitive understanding for the issues of various headlines by creating a wordcloud. 
-# However, the LDA suppliment for the MCMC using Gibbs sampling for the topics of headlines by posterior distributions of sampling techniques by creating its own visualization. 
-# Hence, the topic modeling is accurate for its delivery of topics and sub-words of its each own topics. 
+## Also, the wordcloud delivers for the beauty of frequncies and its intuitive understanding for the issues of various headlines by creating a wordcloud.
+
+## BMB: once again, I'm not sure that this visualization is worth the effort. You've managed to draw a figure that reflects only 6 data points (why don't I see the smaller provinces?) Should we be viewing per capita mortality rather than total mortality?
+
+###
+
+# However, the LDA suppliment for the MCMC using Gibbs sampling for the topics of headlines by posterior distributions of sampling techniques by creating its own visualization.
+# Hence, the topic modeling is accurate for its delivery of topics and sub-words of its each own topics.
 # Moreover, the direct instantaneous delivery of shiny for quantitative modeling on death frequencies also suppliment the importance of data mining where the data could be updated daily.
-# Furthermore, the interactive modeling is unique and complete for its own graph where many things and ideas could be shown by the tool of shiny technology. 
-# Hence, this completes the perfect graphs of data mining and its usage on both qualitative and quantitative data analysis. 
+# Furthermore, the interactive modeling is unique and complete for its own graph where many things and ideas could be shown by the tool of shiny technology.
+# Hence, this completes the perfect graphs of data mining and its usage on both qualitative and quantitative data analysis.
+
+## BMB: mark 2/3. There's a lot of fancy technical stuff here, but neither of these plots really addresses a scientific or inferential question ("how many deaths are there in each province?" isn't a particularly scientific question).  I also want to emphasize that if you used any code from online sources, even if you adapted it a lot to your example, you should definitely cite those sources. (Also, I'm not sure that scraping the covid-19 web page and plotting the results constitutes "geospatial data mining"; doesn't the covid-19 page give you a way to download the data directly without scraping it?)
+
